8e8d9906636810aa117516f9c05342750d38928d
Youssef <spanya2017@gmail.com>
2024-04-17 16:24:24 -0700

    lol

---
+++ models/city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module for city"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class City(BaseModel, Base):
+    """The city for creating a city object"""
+
+    __tablename__ = "cities"
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref="cities")
+    else:
+        name = ""
+        state_id = ""
